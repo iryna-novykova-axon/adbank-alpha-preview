0fe4469884d02fccf114e0977625fefb089406ef
Iryna Novykova <46107199+iryna-novykova-axon@users.noreply.github.com>
2019-01-24 18:34:27 +0200

    test

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var api = require('./routes/api');
 var view = require('./routes/view');
